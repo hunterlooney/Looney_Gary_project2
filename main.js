c7b1b96576ae4cdedadf91dd4e576432862eec00
Gary W. H. Looney <hunterlooney@fullsail.edu>
2013-04-18 21:55:48 -0400

    Everything working but checkbox

--- main.js
+++ main.js
@@ -10,70 +10,76 @@ var sex = document.getElementById("sex");
 var birthdate = document.getElementById("birthdate");
 var race = document.getElementById("race");
 var height = document.getElementById("height");
-
 var submitButton = document.getElementById("submitButton");
 var clearButton = document.getElementById("clearButton");
-
+var getSelectedGlasses = function () {
+	if(glasses.checked) {
+		value = "Yes";
+	} else {
+		value = "No"
+	}
+	return value;
+};
 
 //Submit Button and Get Data 
+/*
 var getData = function () {
+	getSelectedGlasses();
 	localStorage.setItem("Name", cname.value);
-	localStorage.setItem("Glasses", glasses.value);
+	localStorage.setItem("Glasses", value);
 	localStorage.setItem("Sex", sex.value);
 	localStorage.setItem("Birthdate", birthdate.value);
 	localStorage.setItem("Race", race.value);
 	localStorage.setItem("Height", height.value);
 };
 submitButton.addEventListener("click", getData);
+*/
+
+function saveData () {
+	var id = Math.floor(Math.random()*10000001);
+	var item = {};
+		item.name = ["Name: ", cname.value];
+		item.glasses = ["Glasses: ", getSelectedGlasses];
+		item.sex = ["Sex: ", sex.value];
+		item.birthdate = ["Birthdate: ", birthdate.value];
+		item.race = ["Race: ", race.value];
+		item.height = ["Height: ", height.value];
+	localStorage.setItem(id, JSON.stringify(item));
+	alert("Character Added!");
+};
+submitButton.addEventListener("click", saveData);
 
 //Clear Button
 var clearData = function () {
 	localStorage.clear();
+	alert("Data Cleared!");
+
 };
 clearButton.addEventListener("click", clearData);
 
 
-//Apply JavaScript: Save Data
-//Apply JavaScript: Get Data
-//Apply JavaScript: Array Function
+var getTheData = function () {
+	var makeDivision = document.createElement('div');
+		makeDivision.setAttribute("id", "items");
+	var makeTheList = document.createElement('ul');
+		makeDivision.appendChild(makeTheList);
+		document.body.appendChild(makeDivision);
+	for ( var i = 0, len = localStorage.length; i < len; i++) {
+		var makeli = document.createElement('li');
+			makeTheList.appendChild(makeli);
+		var key = localStorage.key(i);
+		var value = localStorage.getItem(key);
+		var obj = JSON.parse(value);
+		var makeSecondList = document.createElement('ul');
+			makeli.appendChild(makeSecondList);
+		for (var n in obj) {
+			var makeSecondli = document.createElement('li');
+				makeSecondList.appendChild(makeSecondli);
+			var optSecondText = obj[n][0] + " "	+ obj[n][1];
+				makeSecondli.innerHTML = optSecondText;
+		}
+	}
+};
 
-//JavaScript: Save Data
-/*
-Requirements
-Use an external .js file
-Write a function that saves all data input into the form into Local Storage when the form's submit button is clicked.
-This means the values for your input, select, radio or checkboxes etc.
-You should also save a key(random number) for your values(array or object). Each saved entry will be a key:value pair.
-The key should be a random number which will create a unique id.
-The value should be an array or object that contains all the values of your form field data.
-This allows for our form to save multiple submissions into Local Storage without overwriting data.
-Remember you can test your local storage by using the Web Inspector
-*/
-
-//JavaScript: Get Data
-/*
-Requirements
-Once data has been submitted through the form, we can grab the data 
-submitted from Local Storage. List out your saved data from Local Storage 
-on the additem.html document using the Display Data link you created. 
-Make sure you hide the form when you display your data on screen as shown 
-in the example screenshots. You can use these the following screenshots as 
-a guideline. Your project's screen will vary from this example based on your topic.
-*/
-
-//JavaScript: Clear Data (COMPLETED)
-/*
-Requirements
-Create a function that removes all data from local storage.
-Be sure to create a clear data link or button in your app that runs the function that removes all data.
-*/
-
-//JavaScript: Array Function
-/*
-Write an array function(s) that populates at least one select form element. This probably will be for your categories but you may have more select fields for other things depending on the topic you chose. This will REPLACE your static HTML select element(s) so that you can see an alterative way to create HTML elements.
-Remember:
-Be consistent in all your code! Check the rubric before turning in your assignment to make sure you won't miss any points.
-Add comments. At minium include the project name, your name, & your term number at the top of EACH of your documents.
-All projects must be at least 50% completed to receive a grade.
-Do not copy my variable and function names.
-*/
+var displayButton = document.getElementById('displayButton');
+displayButton.addEventListener("click", getTheData);
